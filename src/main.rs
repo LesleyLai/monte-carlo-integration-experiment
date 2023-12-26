0926bf101984c9fe4dbb25db84f2729bee78d54d
Lesley Lai <lesley@lesleylai.info>
2023-12-26 21:40:03 +0800

    1D monte carlo integration and variance estimation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
+extern crate core;
+
+mod variance_estimator;
+
+use crate::variance_estimator::VarianceEstimator;
+use rand::prelude::*;
+use rayon::prelude::*;
+use std::cell::RefCell;
+use thread_local::ThreadLocal;
+
+// Estimate integral from a to b of f(x) dx
+fn monte_carlo_integration(
+    f: impl Fn(f64) -> f64 + Sync,
+    a: f64,
+    b: f64,
+    sample_count: usize,
+) -> f64 {
+    let sum: f64 = (0..sample_count)
+        .into_par_iter()
+        .map_init(thread_rng, |rng, _| f(rng.gen_range(a..=b)))
+        .sum();
+
+    sum * (b - a) / (sample_count as f64)
+}
+
+fn test_monte_carlo_integration(
+    f: impl Fn(f64) -> f64 + Sync + Copy,
+    f_desc: &str,
+    a: f64,
+    b: f64,
+    expected: f64,
+) {
+    println!("Estimate {f_desc}. Expected result: {expected}");
+    for i in 0..8 {
+        let sample_count = 2_usize.pow(i);
+        let tl = ThreadLocal::new();
+
+        (0..128).into_iter().for_each(|_| {
+            let result = monte_carlo_integration(f, a, b, sample_count);
+
+            let cell = tl.get_or(|| RefCell::new(VarianceEstimator::new()));
+            cell.borrow_mut().add(result);
+        });
+
+        let ve = tl.into_iter().fold(VarianceEstimator::new(), |a, b| {
+            VarianceEstimator::merge(&a, &b.borrow())
+        });
+
+        println!(
+            "sample count: {}, mean of means: {:.2}, variance: {:.1e}",
+            sample_count,
+            ve.mean,
+            ve.variance()
+        );
+    }
+    println!("==========");
+}
+
 fn main() {
-    println!("Hello, world!");
+    use std::f64::consts::{E, PI};
+    test_monte_carlo_integration(|x| x * x, "∫ from 0 to 1 of x^2 dx", 0.0, 1.0, 0.33);
+    test_monte_carlo_integration(|x| x.sin(), "∫ from 0 to PI of sin(x) dx", 0.0, PI, 2.0);
+    test_monte_carlo_integration(|x| x.cos(), "∫ from 0 to PI of cos(x) dx", 0.0, PI, 0.0);
+
+    test_monte_carlo_integration(
+        |x| 2.0 / PI.sqrt() * E.powf(-x * x),
+        "Error Function erf(1)",
+        0.0,
+        1.0,
+        0.84,
+    );
 }
---
+++ src/variance_estimator.rs
@@ -0,0 +1,134 @@
+// Variance estimator that uses the Welford’s algorithm
+// Code adapted from https://pbr-book.org/4ed/Utilities/Mathematical_Infrastructure#RobustVarianceEstimation
+#[derive(Copy, Clone, Debug, Default)]
+pub struct VarianceEstimator {
+    pub mean: f64,
+    sum_square_differences: f64,
+    sample_count: i64,
+}
+
+impl VarianceEstimator {
+    pub fn new() -> Self {
+        Self {
+            mean: 0.0,
+            sum_square_differences: 0.0,
+            sample_count: 0,
+        }
+    }
+
+    pub fn add(&mut self, x: f64) {
+        self.sample_count += 1;
+        let delta = x - self.mean;
+        self.mean += delta / (self.sample_count as f64);
+        let delta2 = x - self.mean;
+        self.sum_square_differences += delta * delta2;
+    }
+
+    pub fn variance(&self) -> f64 {
+        if self.sample_count > 1 {
+            self.sum_square_differences / (self.sample_count - 1) as f64
+        } else {
+            0.0
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn relative_variance(&self) -> f64 {
+        if self.sample_count < 1 || self.mean == 0.0 {
+            0.0
+        } else {
+            self.variance() / self.mean
+        }
+    }
+
+    pub fn merge(lhs: &Self, rhs: &Self) -> Self {
+        if rhs.sample_count == 0 {
+            return *lhs;
+        }
+
+        let left_sample_count_f64 = lhs.sample_count as f64;
+        let right_sample_count_f64 = rhs.sample_count as f64;
+        let sample_count = lhs.sample_count + rhs.sample_count;
+
+        let sqr_mean_diff = (rhs.mean - lhs.mean) * (rhs.mean - lhs.mean);
+        let sum_square_differences = lhs.sum_square_differences
+            + rhs.sum_square_differences
+            + sqr_mean_diff * left_sample_count_f64 * right_sample_count_f64
+                / (sample_count as f64);
+        let mean = (left_sample_count_f64 * lhs.mean + right_sample_count_f64 * rhs.mean)
+            / (sample_count as f64);
+
+        Self {
+            mean,
+            sum_square_differences,
+            sample_count,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx_eq::assert_approx_eq;
+    use rayon::prelude::*;
+    use std::cell::RefCell;
+    use thread_local::ThreadLocal;
+
+    #[test]
+    fn test_all_same() {
+        const CONSTANT: f64 = 42.0;
+        let mut ve = VarianceEstimator::new();
+        for _ in 0..100 {
+            ve.add(CONSTANT);
+        }
+        assert_eq!(ve.mean, CONSTANT);
+        assert_eq!(ve.variance(), 0.0);
+        assert_eq!(ve.relative_variance(), 0.0);
+    }
+
+    #[test]
+    fn test_range() {
+        let mut ve = VarianceEstimator::new();
+        // An integer sequence from 0 to 100 has an variance around 841.67
+        (0..100).for_each(|i| ve.add(i as f64));
+
+        assert_eq!(ve.mean, 49.5);
+        assert_approx_eq!(ve.variance(), 841.67, 0.01);
+        assert_approx_eq!(ve.relative_variance(), 841.67 / ve.mean, 0.01);
+    }
+
+    #[test]
+    fn test_merge() {
+        let mut ve1 = VarianceEstimator::new();
+        let mut ve2 = VarianceEstimator::new();
+
+        // An integer sequence from 0 to 200 has an variance around 3350
+        (0..100).for_each(|i| ve1.add(i as f64));
+        (100..200).for_each(|i| ve2.add(i as f64));
+
+        let ve = VarianceEstimator::merge(&ve1, &ve2);
+
+        assert_eq!(ve.mean, 99.5);
+        assert_approx_eq!(ve.variance(), 3350.0, 0.01);
+        assert_approx_eq!(ve.relative_variance(), 3350.0 / ve.mean, 0.01);
+    }
+
+    #[test]
+    fn test_concurrent_accumulate() {
+        let tl = ThreadLocal::new();
+        // An integer sequence from 0 to 10000 has an variance around 8334166.67
+        (0..10000).into_par_iter().for_each(|i| {
+            let mut cell = tl.get_or(|| RefCell::new(VarianceEstimator::new()));
+            cell.borrow_mut().add(i as f64);
+        });
+
+        let variance = tl
+            .into_iter()
+            .fold(VarianceEstimator::new(), |a, b| {
+                VarianceEstimator::merge(&a, &b.borrow())
+            })
+            .variance();
+
+        assert_approx_eq!(variance, 8334166.67, 0.01);
+    }
+}
